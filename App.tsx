f2be06bc4af38404d38af73b1ba5a88850259be9
Okogod <godspowerokoro17@gmail.com>
2026-02-25 10:09:30 +0100

    Add custom colors to nativewind

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@ import "./global.css"
 
 export default function App() {
   return (
-    <View className={`flex-1 bg-white-600  justify-center items-center`}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className={`flex-1 bg-white-600 justify-center items-center`}>
+      <Text className={`text-BlackColor`}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
+    colors: {
+      DarkGreenColor: "#008080",
+      LightGreenColor: "#04DCDC",
+      PinkColor: "#FF6B6B",
+      RedColor: "#ED2020",
+      AshColor: "#F9F9F9",
+      GreyColor: "#666666",
+      BlackColor: "#000000",
+      WhiteColor: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [],
