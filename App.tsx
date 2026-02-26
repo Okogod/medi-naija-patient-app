14a9a835b8158182c60fe9ee5ece5e9793fda3e9
Okogod <godspowerokoro17@gmail.com>
2026-02-26 12:40:04 +0100

    Add src folder, and components, screen, types, utils, sub folders. Add .env file

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
 
 export default function App() {
+
+
   return (
     <View className={`flex-1 bg-white-600 justify-center items-center`}>
       <Text className={`text-BlackColor`}>Open up App.tsx to start working on your app!</Text>
